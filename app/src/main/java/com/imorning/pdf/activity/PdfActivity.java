593a28e3c11624a385aa43cb5d0415cf6dac3623
iMorning <admin@catcompany.cn>
2021-03-18 23:11:11 +0800

    Update PdfActivity.java

--- app/src/main/java/com/imorning/pdf/activity/PdfActivity.java
+++ app/src/main/java/com/imorning/pdf/activity/PdfActivity.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
 import com.imorning.pdf.R;
 
 public class PdfActivity extends AppCompatActivity {
-
+    private PDFView pdfView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
